7360dcfd35bf147b3a0a6fd923dc7cd062cdb342
Crowdbotics <engineering@crowdbotics.com>
2024-05-09 14:53:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "simple-todo-list-app-2",
+  url: "https://simple-todo-list-app-2.botics.co",
+  api: "https://simple-todo-list-app-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
